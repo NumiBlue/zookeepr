e258bdf62a598febf19a308eca800e7c1e244d23
NumiBlue <thekindlyblog@gmail.com>
2022-03-28 12:03:39 -0500

    id missing

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -47,6 +53,17 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -64,11 +81,17 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
 
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
-
-  //comment
-
-  
